0d668165606c245b7603af49de1316c67c9293d8
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-26 16:38:41 -0500

    client.js file set up

---
+++ src/client.js
@@ -0,0 +1,16 @@
+import { SanityClient } from "@sanity/client";
+import { ImageUrlBuilder } from "@sanity/image-url";
+
+
+export const client = SanityClient({
+    projectId: process.env.REACT_APP__SANITY_PROJECT_ID, 
+    dataset: 'production', 
+    apiVersion: '2023-02-01', 
+    useCdn: true, 
+    token: process.env.REACT_APP_SANITY_TOKEN
+});
+
+const builder = ImageUrlBuilder(client);
+
+
+export const urlFor = (source) => builder.image(source);
