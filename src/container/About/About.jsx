02239faa13cf3f4d8a25d303ccc130a7c86e8d09
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-26 16:01:52 -0500

    abouts item added

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -8,7 +8,8 @@ import { images } from '../../constants';
 const abouts = [
     { title: 'Fontend Development', description: 'I am a good frontend developer.', imgUrl: images.about01},
     { title: 'Backend Development', description: 'I am a good backend developer', imgUrl: images.about02},
-    { title: 'Web Animations', description: 'I am a good web animations developer', imgUrl: images.about03}
+    { title: 'Web Animations', description: 'I am a good web animations developer', imgUrl: images.about03},
+    { title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about04}
 ]
 const About = () => {
     return (
