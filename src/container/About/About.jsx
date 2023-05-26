f6f6ab592f92997697553076dafac9519c0e66e2
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-26 15:35:29 -0500

    hooks imported

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './About.scss';
+import { motion } from 'framer-motion';
 
 const About = () => {
     return (
