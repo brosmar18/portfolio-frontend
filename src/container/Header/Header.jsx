6369f7eae04394efe3094358f57040782e5bbcd3
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-26 14:12:34 -0500

    scaleVariants added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,6 +4,17 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+    whileInView: {
+        scale: [0, 1], 
+        opacity: [0, 1], 
+        transition: {
+            duration: 1, 
+            ease: 'easeInOut',
+        }
+    }
+};
+
 const Header = () => {
     return(
         <div className='app__header app__flex'>
@@ -42,7 +53,17 @@ const Header = () => {
                     className='overlay_circle'
                 />
             </motion.div>
-            <motion.div>
+            <motion.div
+                variants={scaleVariants}
+                whileInView={scaleVariants.whileInView}
+                className='app__header-circles'
+                >
+                {[images.flutter, images.redux, images.sass].map((circle, index) => (
+                    <div className='circle-cmp app__flex' key={`circle-${index}`}>
+                        <img src={circle} alt='profile_bg' />
+                    </div>
+                ))}
+            
 
             </motion.div>
         </div>
