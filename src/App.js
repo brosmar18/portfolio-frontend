7a8ae7dde13aa764b433a6a0a8383bcb4c5cff65
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-25 23:31:59 -0500

    components added to return()

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className="app">
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
     );
 }
 
