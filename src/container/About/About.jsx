7032815bce407a9b5953331ddf1b7fc0f502202d
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-26 15:39:45 -0500

    abouts array created

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,9 +2,27 @@ import React, { useState, useEffect} from 'react';
 import './About.scss';
 import { motion } from 'framer-motion';
 
+
+
+const abouts = [
+    { title: 'Fontend Development', description: 'I am a good frontend developer.', imgUrl: ''},
+    { title: 'Backend Development', description: 'I am a good backend developer', imgUrl: ''},
+    { title: 'Web Animations', description: 'I am a good web animations developer', imgUrl: ''}
+]
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <h2 className='head-text'>
+                I Know That
+                <span>Good Design</span>
+                <br />
+                means
+                <span>Good Business</span>
+            </h2>
+            <div className='app__profiles'>
+
+            </div>
+        </>
     );
 }
 
