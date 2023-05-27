08dbe5477bb69d2b1454f7d901177a1007b1212c
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-27 13:38:43 -0500

    basic skills component set up

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import ReactTolltip from 'react-tooltip';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
+import client, { urlFor } from '../../client';
 import './Skills.scss';
+
 
 const Skills = () => {
     return (
-        <div>Skills</div>
+        <h2 className='head-text'>Skills & Experience</h2>
     );
 }
 
