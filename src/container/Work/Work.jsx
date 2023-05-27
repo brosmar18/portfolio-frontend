21ef361d81471a181624a1c313e193118aeab11d
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-27 01:05:42 -0500

    onClick syntax error fixed

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -19,8 +19,10 @@ const Work = () => {
             <div className='app__work-filter'>
                 {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
                     <div key={index}
-                    onClick={() => handleWorkFilter(item)}> 
-                    className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+                    onClick={() => handleWorkFilter(item)} 
+                    className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}>
+                        
+                        {item}
                     </div>
                 ))}
             </div>
