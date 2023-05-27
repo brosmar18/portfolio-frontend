9d0ad4931023764c461936de34c24d5d6b19cfd5
Bryan O. Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-26 23:56:37 -0500

    AppWrap HOC created

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
 
-const AppWrap = () => {
+const AppWrap = (Component, idName, classNames) => function HOC() {
     return (
-        <div>AppWrap</div>
-    );
+        <div id={idName} className={`app__container ${classNames}`}>
+            <SocialMedia />
+
+            <div className='app__wrapper app__flex'>
+                <Component />
+            </div>
+        </div>
+    )
 }
 
 export default AppWrap;
