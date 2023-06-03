4f2d3f0256b52c92713a31cc00970aac51685b57
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-03 13:42:22 -0500

    fixed projectID bug that prevented ID and token being loaded from .env file

--- src/client.js
+++ src/client.js
@@ -1,15 +1,17 @@
 import { createClient } from "@sanity/client";
 import ImageUrlBuilder from "@sanity/image-url";
 
-console.log(process.env.REACT_APP_SANITY_PROJECT_ID);
+
+
 const client = createClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID, 
-    dataset: 'production', 
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: 'production',
     apiVersion: '2023-05-26',
     useCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN,
     ignoreBrowserTokenWarning: true
 });
+
 
 const builder = ImageUrlBuilder(client);
 
