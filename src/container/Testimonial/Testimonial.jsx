69f498ab5cb61deeb3814459f3ecf69faedca128
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-05 16:17:59 -0500

    brands added

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import client, { urlFor } from '../../client';
 
@@ -57,6 +57,18 @@ const Testimonial = () => {
                     </div>
                 </>
             )}
+
+            <div className='app__testimonial-brands app__flex'>
+                {brands.map((brand) => (
+                    <motion.div
+                        whileInView={{opacity: [0, 1]}}
+                        transition={{duration: 0.5, type: 'tween'}}
+                        key={brand._id}
+                    >
+                        <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+                    </motion.div>
+                ))}
+            </div>
         </>
     );
 }
