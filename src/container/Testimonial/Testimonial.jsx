39a6ca47ebc63d9bb42c4c2c844fac4475e876b4
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-05 16:10:29 -0500

    onClick added

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import client, { urlFor } from '../../client';
 
@@ -28,13 +28,16 @@ const Testimonial = () => {
         });
 
     }, []);
+    console.log(testimonials[currentIndex]?.imgUrl);
+    console.log("array", testimonials[0]);
+
 
     return (
         <>
-            {testimonials.length && (
+            {testimonials.length > 0 && testimonials[currentIndex] && (
                 <>
                     <div className="app__testimonial-item app__flex">
-                        <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
+                        <img src={urlFor(testimonials[currentIndex].imgUrl)} alt={testimonials[currentIndex].name} />
                         <div className="app__testimonial-content">
                             <p className="p-text">{testimonials[currentIndex].feedback}</p>
                             <div>
@@ -45,7 +48,7 @@ const Testimonial = () => {
                     </div>
 
                     <div className='app__testimonial-btns app__flex'>
-                        <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length -1 : currentIndex -1)}>
+                        <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
                             <HiChevronLeft />
                         </div>
                         <div className='app__flex' onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
