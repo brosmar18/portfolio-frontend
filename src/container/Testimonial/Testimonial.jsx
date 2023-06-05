1da4bfcb90ee239338be79ebf236aef1846d2825
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-05 11:35:38 -0500

    }testimonial container basic set up

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import { AppWrap, MotionWrapo } from '../../wrapper';
@@ -7,6 +7,10 @@ import { urlFor } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [ testimonials, setTestimonials] = useState([]);
+    const [brands, setBrands] = useState([]);
+
     return (
         <div>Testimonial</div>
     );
