bd13e41f8e635a88c6cbe76b2116771cdbea089f
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-05 11:46:03 -0500

    .fetch code set up to get Sanity Testimonial data

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import { AppWrap, MotionWrapo } from '../../wrapper';
-import { urlFor } from '../../client';
+import client, { urlFor } from '../../client';
 
 import './Testimonial.scss';
 
@@ -10,6 +10,20 @@ const Testimonial = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [ testimonials, setTestimonials] = useState([]);
     const [brands, setBrands] = useState([]);
+
+    useEffect (() => {
+        const query = `*[_type == "testimonials"]`;
+        const brandsQuery = `*[_type == "brands"]`;
+
+        client.fetch(query).then((data) => {
+            setTestimonials(data);
+        });
+
+        client.fetch(brandsQuery).then((data) => {
+            setBrands(data);
+        });
+
+    }, []);
 
     return (
         <div>Testimonial</div>
