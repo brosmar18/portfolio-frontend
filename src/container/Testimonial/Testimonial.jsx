db94c3c46fe9adfb2c94f638f064f7c60bf774ec
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-05 15:42:22 -0500

    hiChevronLeft added

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -43,6 +43,12 @@ const Testimonial = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className='app__testimonial-btns app__flex'>
+                        <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length -1 : currentIndex -1)}>
+                            <HiChevronLeft />
+                        </div>
+                    </div>
                 </>
             )}
         </>
