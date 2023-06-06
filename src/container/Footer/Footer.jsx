afc9870fe44f6e855be908325b4f5ff6e751cda4
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-06 16:47:26 -0500

    name, email, and message destructuring setup. handleSubmit function setup

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
-import {client} from '../../client';
+import client, { urlFor } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
@@ -9,10 +9,28 @@ const Footer = () => {
     const [isFormSubmitted, setIsFormSubmitted] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const { name, email, message } = formData;
+
     const handleChangeInput = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     };
+
+    const handleSubmit = () => {
+        setLoading(true);
+
+        const contact = {
+            _type: 'contact',
+            name: formData.username,
+            email: formData.email,
+            message: formData.message,
+        }
+
+        client.create(contact).then(() => {
+            setLoading(false);
+            setIsFormSubmitted(true);
+        })
+    }
 
     return (
         <>
