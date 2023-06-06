3f3e814cf1378c59e2229616b99ab16caf7e920b
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-05 20:02:36 -0500

    email and phone added

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -5,10 +5,23 @@ import {client} from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-    return(
-        <div>Footer</div>
+    return (
+        <>
+            <h2 className='head-text'>Take a coffee & chat with me</h2>
+            <div className='app__footer-cards'>
+                <div className='app__footer-card'>
+                    <img src={images.email} alt='email' />
+                    <a href="mailto:hello@email.com" className='p-text'>hello@email.com</a>
+                </div>
+                <div className='app__footer-card'>
+                    <img src={images.phone} alt='phone' />
+                    <a href="tel:319-999-9999" className='p-text'>(319) 888-8888</a>
+                </div>
+            </div>
+        </>
     );
 }
+
 
 export default AppWrap(
     MotionWrap(Footer, 'app__footer'),
